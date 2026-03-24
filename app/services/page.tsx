864070525ef63f42dff346099cfc79254a38a7a1
Aldrich K Wooden <khaaliswooden@gmail.com>
2026-03-24 03:13:41 -0500

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 // /services has moved to /solutions
 export default function ServicesPage() {
-  redirect('/solutions')
+  permanentRedirect('/solutions')
 }
